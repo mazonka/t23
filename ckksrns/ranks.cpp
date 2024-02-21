2553f762f0fa16eb85c771622c595de7c86ac68a
Your Name <you@example.com>
2024-02-21 20:07:24 +0400

    .

--- ckksrns/ranks.cpp
+++ ckksrns/ranks.cpp
@@ -31,15 +31,14 @@ Integer y(Integer x, int pw)
 
 void cmain()
 {
-    mod = 107;
+    mod = 257;
 
     for ( Integer i = 0; i < mod; i++ )
     {
-        auto j = mod - i;
-        if( i==0 ) j=0;
+        auto j = i?mod - i:0;
         cout << i
              << '\t' << y(i, -1)
-             << '\t' << (y(i, -1)+y(j,-1))
+             //<< '\t' << (mod%(i?i:1))
              << '\t' << y(i, 1)
              << '\t' << y(i, 2)
              << '\t' << y(i, 3)
--- ckksrns/ranks.plt
+++ ckksrns/ranks.plt
@@ -1,7 +1,7 @@
-plot "ranks.dat" u 1:1 w l
-replot "ranks.dat" u 1:2 w l
-replot "ranks.dat" u 1:3 w l
-#replot "ranks.dat" u 1:4 w l
+plot "ranks.dat" u 1:3 w l
+#replot "ranks.dat" u 1:2 w l
+#replot "ranks.dat" u 1:3 w l
+replot "ranks.dat" u 1:4 w l
 #replot "ranks.dat" u 1:5 w l
 #replot "ranks.dat" u 1:6 w l
 
