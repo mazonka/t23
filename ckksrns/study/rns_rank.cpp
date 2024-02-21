2de060b7e96153ddb67320d85c56de38d902f4a3
Your Name <you@example.com>
2024-02-21 18:52:24 +0400

    .

--- ckksrns/study/rns_rank.cpp
+++ ckksrns/study/rns_rank.cpp
@@ -233,13 +233,13 @@ void cmain()
     {
         //Rns rns({13, 15, 17, 19});
         //Rns rns({11, 5, 7});
-        //Rns rns({3, 5, 7});
+        Rns rns({3, 5, 7});
         //const int x = 11, y = 13, z = 15;
         //Rns rns({11, 13, 15});
         //const int x = 17, y = 23, z = 29;
         //Rns rns({17, 23, 29});
         //Rns rns({5, 7, 11});
-        Rns rns({7, 9});
+        //Rns rns({7, 9});
 
         int sz = (int)rns.qs.size();
 
